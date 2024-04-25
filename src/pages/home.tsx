a29ac6f35a84987588c5e85856b3f19cfed9a01c
zeindevs <zeindevs@gmail.com>
2024-04-25 19:17:58 +0700

    init

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<div>Home</div>
+		</div>
+	)
+}
+
+export default Home
---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_DESCRIPTION: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
